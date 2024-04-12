a721fe094f126a9ff3d429d5881f442580bac4bf
Kevin Reys <kevinrsye@gmail.com>
2024-04-12 18:36:59 -0300

    print4

---
+++ PRINT/print4.java
@@ -0,0 +1,10 @@
+public class print4 {
+    public static void main(String[] args) {
+        String nome = "João";
+        int idade = 30;
+        double altura = 1.75;
+
+        // Usando printf para imprimir uma mensagem formatada
+        System.out.printf("Olá, meu nome é %s, tenho %d anos e minha altura é %.2f metros.", nome, idade, altura);
+    }
+}
