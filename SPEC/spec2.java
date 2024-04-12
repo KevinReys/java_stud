bcd05b67e0be4263defadcc941ccff72bb610b0c
Kevin Reys <kevinrsye@gmail.com>
2024-04-12 18:27:10 -0300

    Specifier2

---
+++ SPEC/spec2.java
@@ -0,0 +1,9 @@
+package SPEC;
+
+public class spec2 {
+    public static void main(String[] args) {
+        String nome = "Kevin";
+        System.out.printf("Nome:%s\n", nome);
+    }
+
+}
--- print2.java
+++
@@ -1,9 +0,0 @@
-public class print2 {
-    public static void main(String[] args){
-
-         System.out.println("Hello World!");
-         System.out.println("My name is Kevin");
-         System.out.println("Im 34 years old");
-
-    }
-}
