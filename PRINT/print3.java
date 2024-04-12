8fd58c3aac3700a737e311492cdbc21c82550759
Kevin Reys <kevinrsye@gmail.com>
2024-04-12 18:08:47 -0300

    new archives PRINT

---
+++ PRINT/print3.java
@@ -0,0 +1,11 @@
+public class print3 {public class helloworld_c {
+    public static void main(String[] args) {
+        String nome = "Rafaela";
+        int idade = 30;
+        double altura = 1.65;
+
+        System.out.printf("Nome: %s, Idade: %d, Altura: %.2f\n", nome, idade, altura);
+    }
+}
+
+}
