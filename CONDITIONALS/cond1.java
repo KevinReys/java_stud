067c58461b2afb50173821600cdfa212a080d669
Kevin Reys <kevinrsye@gmail.com>
2024-04-12 23:41:31 -0300

    create new paste CONDITIONALS

---
+++ CONDITIONALS/cond1.java
@@ -0,0 +1,10 @@
+package CONDITIONALS;
+
+public class cond1 {
+    public static void main(String[] args) {
+        if (0 > 10) {
+            System.out.println("maior");
+        }
+        System.out.println("Fim");
+    }
+}
