15ab13302ef784af4811640aafd201ef03bc4bc6
Kevin Reys <kevinrsye@gmail.com>
2024-04-13 13:31:36 -0300

    new file conditional IF ELSE

---
+++ CONDITIONALS/cond2.java
@@ -0,0 +1,16 @@
+package CONDITIONALS;
+
+public class cond2 {
+    public static void main(String[] args) {
+
+        int nota = 50;
+        int media = 60;
+
+        if (nota >= media) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+        System.out.println("Fim");
+    }
+}
