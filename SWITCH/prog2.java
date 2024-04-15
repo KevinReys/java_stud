efe7930c79e8c569aa0429a84c073785b874b27e
Kevin Reys <kevinrsye@gmail.com>
2024-04-14 21:57:20 -0300

    new file prog2.java

---
+++ SWITCH/prog2.java
@@ -0,0 +1,27 @@
+package SWITCH;
+
+public class prog2 {
+    public static void main(String[] args) {
+        int pos = 1;
+
+        switch (pos) {
+            case 1:
+                System.out.println("Primeiro lugar");
+                break;
+
+            case 2:
+                System.out.println("Segundo lugar");
+                break;
+
+            case 3:
+                System.out.println("Terceiro lugar");
+                break;
+
+            default:
+                System.out.println("Não subiu ao pódio");
+                break;
+        }
+    }
+}
+
+
