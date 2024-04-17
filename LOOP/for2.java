645226b5c45761dbcc5e1e21fab5d6f4dab196bd
Kevin Reys <kevinrsye@gmail.com>
2024-04-16 23:46:07 -0300

    for2

---
+++ LOOP/for2.java
@@ -0,0 +1,12 @@
+package LOOP;
+
+public class for2 {
+    public static void main(String[] args) {
+
+        for (int cont = 5; cont < 0; cont--) {
+            System.out.println("Curso de Java");
+        }
+        System.out.println(cont + " - Fim do programa");
+    }
+
+}
