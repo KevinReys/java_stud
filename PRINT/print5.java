5b48b8c030353dcde90f89f3d445093cb5f8db08
Kevin Reys <kevinrsye@gmail.com>
2024-04-19 15:11:16 -0300

    print5

---
+++ PRINT/print5.java
@@ -0,0 +1,8 @@
+public class print5 {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+
+        System.out.printf("\n\nSoma dos numeros %d", (number1 + number2));
+    }
+}
