d532339fd2ce4c91e95885bc0c0b5a3b173cda73
Kevin Reys <kevinrsye@gmail.com>
2024-04-19 17:52:50 -0300

    IF2

---
+++ IF/FuncaoIf2.java
@@ -0,0 +1,15 @@
+package IF;
+
+public class FuncaoIf2 {
+    public static void main(String[] args) {
+        int number = 5;
+
+        if (number >= 6) {
+            System.out.println("The number is greater than 5");
+
+        } else {
+            System.out.println("The number is less than 5");
+        }
+    }
+
+}
