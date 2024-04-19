511ad44678c46a68f5ef8adfa42b47e43782c24f
Kevin Reys <kevinrsye@gmail.com>
2024-04-19 17:48:30 -0300

    IF

---
+++ IF/FuncaoIf1.java
@@ -0,0 +1,14 @@
+package IF;
+
+public class FuncaoIf1 {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        if (number >= 10) {
+            System.out.println("The number is greater than 10");
+
+        }
+    }
+
+}
