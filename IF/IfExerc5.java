3579503712be4ca62cdef7184774ece576f46c48
Kevin Reys <kevinrsye@gmail.com>
2024-04-22 20:57:13 -0300

    Exerc 5

---
+++ IF/IfExerc5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IfExerc5 {
+    public static void main(String[] args) {
+        Scanner objLogin = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = objLogin.nextLine();
+
+        System.out.println("Enter your password");
+        String senha = objLogin.nextLine();
+
+        if (name.equals("Kevin") && senha.equals("130290")) {
+            System.out.printf("User %s successfully logged in!\n", name);
+        } else {
+            System.out.println("Invalid user or password!");
+        }
+    }
+}
