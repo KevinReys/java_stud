05570e15ae34bc15c84b54e4ca5150451a0b0386
Kevin Reys <kevinrsye@gmail.com>
2024-04-24 23:28:39 -0300

    FOR1

---
+++ EstruturasdeRepeticao/For.java
@@ -0,0 +1,28 @@
+package EstruturasdeRepeticao;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        // for - Para
+        for (int uva = 1; uva <= 10; uva++) {
+
+            System.out.println("Numero: " + uva);
+
+        }
+
+        for (int contador = 10; contador >= 1; contador--) {
+
+            System.out.println("Numero: " + contador);
+
+        }
+
+        for (int contador = 2; contador <= 10; contador += 2) {
+
+            System.out.println("Numero: " + contador);
+
+        }
+
+    }
+
+}
